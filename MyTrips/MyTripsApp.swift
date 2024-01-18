1a8ceeea76c50aa267c2d716b9d4810c0c9ea62d
Jacek Kosinski <kashiash@gmail.com>
2024-01-18 13:36:32 +0100

    Initial Commit

---
+++ MyTrips/MyTripsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTripsApp.swift
+//  MyTrips
+//
+//  Created by Jacek Kosinski U on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyTripsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
