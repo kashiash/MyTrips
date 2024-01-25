3fcee317d4fd0fda05d83f4cda59e18c9fbbbdf4
Jacek Kosinski <kashiash@gmail.com>
2024-01-25 16:16:53 +0100

    locartion

---
+++ MyTrips/DestinationsTab/DestinationLocationsMapView2.swift
@@ -0,0 +1,8 @@
+//
+//  DestinationLocationsMapView2.swift
+//  MyTrips
+//
+//  Created by Jacek Kosinski U on 25/01/2024.
+//
+
+import Foundation
---
+++ MyTrips/SharedViews/LocationDeniedView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDeniedView.swift
+//  MyTrips
+//
+//  Created by Jacek Kosinski U on 25/01/2024.
+//
+
+import SwiftUI
+
+struct LocationDeniedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDeniedView()
+}
